213c97c4707615842d1776303873e118802223ca
Hendrik Luyckx <hendrik@Hendriks-MacBook-Air.local>
2014-12-08 14:36:01 +0100

    Initial Commit

---
+++ Postkaartje/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Postkaartje
+//
+//  Created by Hendrik Luyckx on 08/12/14.
+//  Copyright (c) 2014 hendrikluyckx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
